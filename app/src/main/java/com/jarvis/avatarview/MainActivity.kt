baf1c178cd3cf9bb721b0fd7f74e1c164ce0ac58
thangnc <thangnc@ominext.com>
2022-04-15 13:18:40 +0700

    chore

--- app/src/main/java/com/jarvis/avatarview/MainActivity.kt
+++ app/src/main/java/com/jarvis/avatarview/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val avatarView = this.findViewById<ViewAvatar>(R.id.avatar)
         avatarView.setDataAvatar(false, "L D", "")
+
     }
 }
