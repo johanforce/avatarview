bdccd3a2f0dc8a0b789710fbdc65302b0c4bc5ec
thangnc <thangnc@ominext.com>
2022-08-03 11:06:33 +0700

    test save lint 2

--- app/src/main/java/com/jarvis/avatarview/MainActivity.kt
+++ app/src/main/java/com/jarvis/avatarview/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val avatarView = this.findViewById<ViewAvatar>(R.id.avatar)
-        avatarView.setDataAvatar(false, "L V V VV V D", "");
-        val text = "Thang"
-        Log.i("TAG", "----------"+ text);
+        avatarView.setDataAvatar(false, "L V V VV V D", "")
+        var text = "Thang"
+        Log.i("TAG", "----------"+ text)
     }
 }
